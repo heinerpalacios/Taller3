a7bc4d2f38778bb5223d00b5f08e847c400a3a16
heinerpalacios <98869007+heinerpalacios@users.noreply.github.com>
2022-02-09 18:42:00 -0500

    Create Library.java
    Creacion de la clase Abstracta Libreria y sus comportamientos

---
+++ src/taller3/domain/Library.java
@@ -0,0 +1,30 @@
+
+/**
+ *
+ * @author HEINER PALACIOS ASPRILLA
+ */
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Heiner Palacios Asprilla
+ */
+public abstract class Library {
+     private ArrayList songs = new ArrayList();
+
+     public abstract void filtarCancionesGenero();
+     public abstract void filtarCancionesAno();
+     public abstract void ordenarDuracion();
+     public abstract void ordenaFecha();
+     
+     
+      public ArrayList getSongs() {
+        return this.songs;
+    }
+
+    public void setSongs(ArrayList songs) {
+        this.songs = songs;
+    }
+     
+}
